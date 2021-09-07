180dd62ee8e5d2cf2219ea764b3bec65b11e2fec
oryx1729 <78848855+oryx1729@users.noreply.github.com>
2021-09-07 12:27:21 +0200

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -5,15 +5,14 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+import requests
 
 
-class ActionHelloWorld(Action):
+class ActionHaystack(Action):
 
     def name(self) -> Text:
         return "call_haystack"
@@ -21,7 +20,19 @@ class ActionHelloWorld(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        raise ZeroDivisionError
-        dispatcher.utter_message(text="Haystack Action!")
+
+        url = "http://localhost:8000/query"
+        payload = {"query": str(tracker.latest_message["text"])}
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        response = requests.request("POST", url, headers=headers, json=payload).json()
+
+        if response["answers"]:
+            answer = response["answers"][0]["answer"]
+        else:
+            answer = "No Answer Found!"
+
+        dispatcher.utter_message(text=answer)
 
         return []
