e8f1d672529be948e3e0d32b657984560fb47070
Greg Stephens <greg@udon.org>
2022-12-08 10:26:14 -0800

    update requirements.txt

--- actions/actions.py
+++ actions/actions.py
@@ -10,7 +10,6 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import aiohttp
-import asyncio
 import json
 import logging
 
