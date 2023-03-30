2df69169a65ef853c5b052b216fe53a5636e435f
Greg Stephens <greg@udon.org>
2023-03-30 13:50:39 -0700

    Update build.sh
    Co-authored-by: Tobias Wochinger <mail@tobias-wochinger.de>

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Build action server image
 VERS=3.3.0
-REGISTRY=stephens
+REGISTRY=<Docker organization>
 IMAGE=haystack-action
 
 time docker buildx build --platform linux/amd64,linux/arm64 --output=type=registry --tag ${REGISTRY}/${IMAGE}:${VERS} --tag ${REGISTRY}/${IMAGE}:latest .
